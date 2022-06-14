646af911be7e2473135e8bb26b177261ab7b9d6a
hwrim <tnekdla@naver.com>
2022-06-14 23:47:05 +0900

    msa jpa

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,8 +1,10 @@
 package com.example.userservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +13,6 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
+
 
 }
