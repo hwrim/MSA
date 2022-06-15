f92eaf3e9f8b2d826ab9c62187d3c0925d3118b4
hwrim <tnekdla@naver.com>
2022-06-15 23:41:01 +0900

    msa users add

--- user-service/src/main/java/com/example/userservice/jpa/UserEntity.java
+++ user-service/src/main/java/com/example/userservice/jpa/UserEntity.java
@@ -14,6 +14,8 @@ public class UserEntity {
     @Column(nullable = false, length = 50, unique = true)
     private String email;
     @Column(nullable = false, length = 50)
+    private String name;
+    @Column(nullable = false, length = 50)
     private String pwd;
     @Column(nullable = false, unique = true)
     private String userId;
